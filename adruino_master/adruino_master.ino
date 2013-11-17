da4b309db557fe21d2e1c5ed96e0316994b6662b
Sergey Pronin <q.pronin@gmail.com>
2013-11-16 19:32:03 -0800

    ble + serial now can write numbers

--- adruino_master/adruino_master.ino
+++ adruino_master/adruino_master.ino
@@ -1,27 +1,61 @@
 #include <Wire.h>
+#include <SPI.h>
+#include <boards.h>
+#include <ble_shield.h>
+#include <services.h> 
 
 int DOLLAR = 0x24;
 
+unsigned char buf[16] = {0};
+unsigned char len = 0;
+
 void setup() {
-  Wire.begin();  
+  Wire.begin();
+  ble_begin();
+  
+  Serial.begin(57600);  
 }
 
+int numbers = 0;
+
 void loop() {
-  Wire.beginTransmission(7);
-  Wire.write(DOLLAR);
-  Wire.endTransmission();
+  while ( ble_available() ) {
+   sendValue(ble_read());
+  }
   
-  Wire.beginTransmission(8);
-  Wire.write(0x31);
-  Wire.endTransmission();
+  while(Serial.available()) {
+    sendValue(Serial.read());
+  }
   
-  Wire.beginTransmission(9);
-  Wire.write(0x32);
-  Wire.endTransmission();
+  numbers = 0;
   
-  Wire.beginTransmission(10);
-  Wire.write(0x33);
+  ble_do_events();
+}
+
+void sendValue(int value) {
+  if (numbers == 0) {
+     Wire.beginTransmission(7);
+     Wire.write(DOLLAR);
+     Wire.endTransmission();
+   }
+   sendNumber(value);
+   numbers++;
+}
+
+void sendNumber(int number) {
+  int output;
+  switch(numbers) {
+    case 0:
+      output = 8;
+      break;
+    case 1:
+      output = 9;
+      break;
+    case 2:
+      output = 10;
+      break;
+  }
+  Wire.beginTransmission(output);
+  Wire.write(0x30 + number-48);
   Wire.endTransmission();
-  
-  delay(5000);
 }
