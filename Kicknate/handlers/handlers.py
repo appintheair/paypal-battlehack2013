2362368835c173511bec8103588d63c10f4f3a81
Bayram.Annakov <bayram.annakov@gmail.com>
2013-11-17 07:15:53 -0800

    Small fix

--- Kicknate/handlers/handlers.py
+++ Kicknate/handlers/handlers.py
@@ -8,8 +8,8 @@ class CreateEntitites(webapp2.RequestHandler):
         donation.donation_id = "3731A944-09AA-E79E-2E54-0A27DEB925F8"
         donation.amount = 1000
         donation.photo1_url = "http://www.empatika.com/blog/wp-content/uploads/2013/11/pic1_new.jpg"
-        donation.photo1_url = "http://www.empatika.com/blog/wp-content/uploads/2013/11/pic2_new.jpg"
-        donation.photo1_url = "http://www.empatika.com/blog/wp-content/uploads/2013/11/pic3_new.jpg"
+        donation.photo2_url = "http://www.empatika.com/blog/wp-content/uploads/2013/11/pic2_new.jpg"
+        donation.photo3_url = "http://www.empatika.com/blog/wp-content/uploads/2013/11/pic3_new.jpg"
         donation.physical_address = "E9:B4:F3:9E:23:B2"
         donation.title = "Food for Homeless and Homebound"
 
