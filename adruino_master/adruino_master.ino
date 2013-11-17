7aed43b6634135a45eb3af7d2e877ccbfeb1f8c5
Sergey Pronin <q.pronin@gmail.com>
2013-11-16 21:46:31 -0800

    now bytes are being sent. animation now done on master. some binary magic done :

--- adruino_master/adruino_master.ino
+++ adruino_master/adruino_master.ino
@@ -33,29 +33,41 @@ void loop() {
 }
 
 void sendValue(int value) {
-  if (numbers == 0) {
-     Wire.beginTransmission(7);
-     Wire.write(DOLLAR);
-     Wire.endTransmission();
-   }
-   sendNumber(value);
-   numbers++;
+  int convertedNumber = value-48;
+  if (numbers == 0 && convertedNumber >= 0 && convertedNumber <= 9) {
+     sendNumber(DOLLAR);
+     numbers++;
+  }
+  sendNumber(convertedNumber + 0x30);
+  numbers++;
 }
 
+int digits[] = { -1, -1, -1, -1 };
+
 void sendNumber(int number) {
-  int output;
-  switch(numbers) {
-    case 0:
-      output = 8;
-      break;
-    case 1:
-      output = 9;
-      break;
-    case 2:
-      output = 10;
-      break;
+  int oldNumber = digits[numbers];
+  if (oldNumber >= 0) {
+    animate(oldNumber, true);
   }
+  animate(number, false);
+  digits[numbers] = number;
+}
+
+void animate(int n, bool r) {
+  for (int i=r?1:-8; r?i<8:i<=0; i++) {
+   show(n, i+8); 
+   delay(30);
+  }
+}
+
+void show(int n, int p) {
+  int output = numbers + 7;
   Wire.beginTransmission(output);
-  Wire.write(0x30 + number-48);
+  byte data[4];
+  data[0] = n >> 4;
+  data[1] = n & 0xf;
+  data[2] = p >> 4;
+  data[3] = p & 0xf;
+  Wire.write(data, 4);
   Wire.endTransmission();
 }
--- rainbowduino_slave/rainbowduino_slave.ino
+++ rainbowduino_slave/rainbowduino_slave.ino
@@ -5,19 +5,54 @@ void setup() {
   Rb.init();  
   Rb.blankDisplay();
   
-  Wire.begin(10);
+  Serial.begin(9600);
+  
+  Wire.begin(7);
   delay(2000);
   Wire.onReceive(digitReceive);
+  
 }
 
 void loop() {
-  
+  while(Serial.available()) {
+    int n = Serial.read();
+     displayNumber(n-48);  
+  }
 }
 
+int currentNumber = -1;
+
 void digitReceive(int count) {
-  int number = Wire.read();
+  int number = 0;
+  for (int i=count; i>0; i--) {
+    int val = Wire.read();
+    number += val << 4*(i-1);
+  }
+  displayNumber(number);
+}
+
+void displayNumber(int number) {
+  if (number == currentNumber)
+    return;
   
   Rb.blankDisplay();
-  delay(200);
-  Rb.drawChar(number, 0, 0, 0x00FF00);
+  int val = number & 0x00FF;
+  int x = (val & 0xF0) >> 4;
+  int y = (val & 0x0F) - 8;
+  Rb.drawChar(number >> 8, x, y, 0x00FF00);
+  
+//  if (currentNumber >= 0) {
+//    animateNumber(currentNumber, true);
+//  }
+//  currentNumber = number;
+//  animateNumber(number, false);
+//  delay(500);
+}
+
+void animateNumber(int number, bool upDown) {
+//  for (int i=upDown?1:-7; upDown?i<8:i<=0; i++) {
+//    Rb.blankDisplay();
+//    Rb.drawChar(number, 0, i, 0x00FF00);
+//    delay(50);
+//  }
 }
