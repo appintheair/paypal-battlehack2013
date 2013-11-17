35f1747296ae2a75c5f18bf34f523433709ad4c1
Alexander Balaban <aabalaban_1@edu.hse.ru>
2013-11-17 14:11:29 +0400

    second view

--- BattlehackHackIOSClient/BattlehackHackIOSClient/BHViewController.m
+++ BattlehackHackIOSClient/BattlehackHackIOSClient/BHViewController.m
@@ -23,11 +23,30 @@
     BHUpdateDonationDetails *_updateDetailsHandler;
     __weak NSString *_currentUUID;
     NSString *_payerEmail;
-    NSInteger _amountLeft;
+    NSInteger _amount;
+    NSInteger _donatorsNumber;
+    NSInteger _donation;
     __weak IBOutlet UILabel *_currentAmount;
     IBOutlet UIView *_dummyView;
+    __weak IBOutlet UIButton *_twentyDollarsDonationButton;
+    __weak IBOutlet UIButton *_tenDollarsDonationButton;
+    __weak IBOutlet UIButton *_customDonationButton;
     
     PayPalPaymentViewController *_paypalController;
+    
+    // second layer view
+    UIView *_filterView2;
+    UILabel *_donateLabel1;
+    UILabel *_donateLabel2;
+    UIView *_dummyView1;
+    UIView *_dummyView2;
+    UILabel *_donationLabel;
+    UILabel *_nTitleLabel;
+    __weak IBOutlet UIButton *_doneButton;
+    
+    // third layer view
+    UILabel *_thankYouLabel;
+    __weak IBOutlet UIButton *_inviteButton;
 }
 
 @end
@@ -56,15 +75,49 @@
     [self.view addSubview:_filterView];
     [self.view addSubview:_pageControl];
     _titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 230, 160, 60)];
-    [_titleLabel setFont:[UIFont fontWithName:@"PT Sans" size:25.f]];
+    [_titleLabel setFont:[UIFont fontWithName:@"PT Sans" size:15.f]];
     [_titleLabel setTextColor:[UIColor whiteColor]];
     [_titleLabel setBackgroundColor:[UIColor clearColor]];
     [_titleLabel setNumberOfLines:2];
-    [_titleLabel setAutoresizesSubviews:YES];
+    [_titleLabel setAdjustsFontSizeToFitWidth:YES];
     [self.view addSubview:_titleLabel];
-    [_numberOfDonators setFont:[UIFont fontWithName:@"PT Sans" size:15.f]];
-    [_currentAmount setFont:[UIFont fontWithName:@"PT Sans" size:25.f]];
+    [_numberOfDonators setFont:[UIFont fontWithName:@"PT Sans" size:20.f]];
+    [_currentAmount setFont:[UIFont fontWithName:@"PT Sans" size:20.f]];
     [_dummyView setBackgroundColor:[UIColor colorWithRed:191./255 green:200./255 blue:207./255 alpha:1.]];
+    
+    // second layer
+    _filterView2 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 320)];
+    [_filterView2 setBackgroundColor:[UIColor colorWithRed:20./255 green:75./255 blue:93./255 alpha:0.9]];
+    _donateLabel1 = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, 320, 50)];
+    [_donateLabel1 setTextAlignment:NSTextAlignmentCenter];
+    [_donateLabel1 setFont:[UIFont fontWithName:@"PT Sans" size:30.f]];
+    [_donateLabel1 setTextColor:[UIColor whiteColor]];
+    [_donateLabel1 setText:@"I donate"];
+    _donateLabel2 = [[UILabel alloc] initWithFrame:CGRectMake(0, 130, 320, 50)];
+    [_donateLabel2 setTextAlignment:NSTextAlignmentCenter];
+    [_donateLabel2 setFont:[UIFont fontWithName:@"PT Sans" size:30.f]];
+    [_donateLabel2 setTextColor:[UIColor whiteColor]];
+    [_donateLabel2 setText:@"for"];
+    _dummyView1 = [[UIView alloc] initWithFrame:CGRectMake(105, 65, 110, 2)];
+    [_dummyView1 setBackgroundColor:[UIColor whiteColor]];
+    _dummyView2 = [[UIView alloc] initWithFrame:CGRectMake(105, 130, 110, 2)];
+    [_dummyView2 setBackgroundColor:[UIColor whiteColor]];
+    _donationLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 65, 320, 70)];
+    [_donationLabel setFont:[UIFont fontWithName:@"PT Sans" size:50.f]];
+    [_donationLabel setTextColor:[UIColor whiteColor]];
+    [_donationLabel setTextAlignment:NSTextAlignmentCenter];
+    _nTitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(40, 140, 240, 180)];
+    [_nTitleLabel setNumberOfLines:3];
+    [_nTitleLabel setTextAlignment:NSTextAlignmentCenter];
+    [_nTitleLabel setFont:[UIFont fontWithName:@"PT Sans" size:35.f]];
+    [_nTitleLabel setTextColor:[UIColor whiteColor]];
+    [_doneButton setHidden:YES];
+    
+     // third layer
+    _thankYouLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320, 320)];
+    [_thankYouLabel setTextAlignment:NSTextAlignmentCenter];
+    [_thankYouLabel setText:@"Thank you!"];
+    [_thankYouLabel sizeToFit];
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -85,6 +138,8 @@
         [_numberOfDonators setText:[NSString stringWithFormat:@"%@", [response objectForKey:@"numberOfVoters"]]];
         [_titleLabel setText:[response objectForKey:@"title"]];
         NSNumber *value = [response objectForKey:@"amountRaised"];
+        _amount = [value integerValue];
+        _donatorsNumber = [[response objectForKey:@"numberOfVoters"] integerValue];
         [_currentAmount setText:[NSString stringWithFormat:@"$%ld", (long)[value integerValue]]];
         UIImageView *view1 = [[UIImageView alloc] init];
         UIImageView *view2 = [[UIImageView alloc] init];
@@ -144,8 +199,44 @@
                                {
                                    completionBlock(NO, nil);
                                }
-                           }];
+                }];
 }
+
+- (void)switchToSecondLayer
+{
+    [_filterView setAlpha:0.];
+    [self.view addSubview:_filterView2];
+    [_filterView2 addSubview:_donateLabel1];
+    [_filterView2 addSubview:_dummyView1];
+    [_filterView2 addSubview:_dummyView2];
+    [_donationLabel setText:[NSString stringWithFormat:@"$%d", _donation]];
+    [_filterView2 addSubview:_donationLabel];
+    [_filterView2 addSubview:_donateLabel2];
+    [_nTitleLabel setText:[_titleLabel text]];
+    [_filterView2 addSubview:_nTitleLabel];
+    [_doneButton setHidden:NO];
+    [_numberOfDonators setText:[NSString stringWithFormat:@"%d", _donatorsNumber]];
+    [_donationLabel setText:[NSString stringWithFormat:@"$%d", _donation]];
+    [_currentAmount setText:[NSString stringWithFormat:@"$%d", _amount]];
+    [_tenDollarsDonationButton setHidden:YES];
+    [_customDonationButton setHidden:YES];
+    [_twentyDollarsDonationButton setHidden:YES];
+}
+
+- (void)switchToThirdLayer
+{
+    [_donateLabel1 removeFromSuperview];
+    [_dummyView1 removeFromSuperview];
+    [_dummyView2 removeFromSuperview];
+    [_donationLabel removeFromSuperview];
+    [_donateLabel2 removeFromSuperview];
+    [_nTitleLabel removeFromSuperview];
+    [_doneButton setHidden:YES];
+    
+    [_filterView2 addSubview:_thankYouLabel];
+    
+}
+
 
 #warning extract payment method in the near future
 - (void)makePayment:(NSString *)amount
@@ -153,7 +244,7 @@
     PayPalPayment *payment = [[PayPalPayment alloc] init];
     payment.amount = [[NSDecimalNumber alloc] initWithString:amount];
     payment.currencyCode = @"USD";
-    payment.shortDescription = @"Super example";
+    payment.shortDescription = [_titleLabel text];
     
 #warning it woudn't work on live!
     [PayPalPaymentViewController setEnvironment:PayPalEnvironmentNoNetwork];
@@ -198,14 +289,20 @@
     [_updateDetailsHandler updateDonationDetailsWithID:currentUUID AndAmount:completedPayment.amount ByUser:email WithCompletionBlock:^{
         
     }];
-    NSInteger value = _amountLeft - [completedPayment.amount longValue];
-    [_controller pushToDevice:[NSString stringWithFormat:@"%ld", (long)value]];
+    [_controller pushToDevice:[NSString stringWithFormat:@"%ld", (long)_amount]];
+    _donatorsNumber += 1;
+    _donation = [completedPayment.amount integerValue];
+    _amount += [completedPayment.amount integerValue];
+    [self switchToSecondLayer];
 }
 
 - (void)payPalPaymentDidCancel
 {
     [_paypalController dismissViewControllerAnimated:YES completion:^{
-        
+        _donatorsNumber += 1;
+        _donation = 23;
+        _amount += 123;
+        [self switchToSecondLayer];
     }];
 }
 
