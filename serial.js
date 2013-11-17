dbc583df57b4c24fc2b41b6a4da24866a08adf95
Sergey Pronin <q.pronin@gmail.com>
2013-11-17 09:18:05 -0800

    Sync info with arduino

--- serial.js
+++ serial.js
@@ -1,5 +1,9 @@
 var connectionId;
 
+var currentState;
+var physicalAddress;
+var uuid;
+
 function serialPut(value) {
     value = parseInt(value);
     var buffer = new ArrayBuffer(3);
@@ -8,6 +12,40 @@ function serialPut(value) {
     uint8View[1] = ((value / 10) % 10) + 48;
     uint8View[2] = (value % 10) + 48;
     chrome.serial.write(connectionId, buffer, function() {});
+}
+
+function syncPhysical() {
+    currentState = 1;
+    physicalAddress = '';
+    var buffer = new ArrayBuffer(1);
+    var uint8View = new Uint8Array(buffer);
+    uint8View[0] = 1;
+    chrome.serial.write(connectionId, buffer, function() {});
+
+    setTimeout(syncUuid, 3000);
+}
+
+function syncUuid() {
+    currentState = 2;
+    uuid = '';
+    var buffer = new ArrayBuffer(1);
+    var uint8View = new Uint8Array(buffer);
+    uint8View[0] = 2;
+    chrome.serial.write(connectionId, buffer, function() {});
+
+    setTimeout(displaySyncResult, 3000);
+}
+
+function displaySyncResult() {
+
+    $('input[name="physical_address"]').val(physicalAddress);
+    $('input[name="uuid"]').val(uuid);
+
+    currentState = 3;
+
+    $('#beacon_ready').css({'display': 'none'});
+    $('#beacon_present').css({'display': 'block'});
+    $('#beacon_button').css({'display': 'block'});
 }
 
 function onSerialOpen(openInfo) {
@@ -20,14 +58,31 @@ function onSerialOpen(openInfo) {
     console.log(openInfo)
 
     $('#beacon_none').css({'display': 'none'});
-    $('#beacon_present').css({'display': 'block'});
-    $('#beacon_button').css({'display': 'block'});
+    $('#beacon_ready').css({'display': 'block'});
+
+    chrome.serial.read(connectionId, 1, onSerialRead);
+    setTimeout(syncPhysical, 3000);
 }
 
 function openPort(port) {
     chrome.serial.open(port, onSerialOpen);
 }
 
+function onSerialRead(readInfo) {
+    var uint8View = new Uint8Array(readInfo.data);
+    for (var i=0; i<readInfo.bytesRead; i++) {
+        var value = String.fromCharCode(uint8View[i]);
+        if (currentState == 1) {
+            physicalAddress += value;
+        } else if (currentState == 2) {
+            uuid += value;
+        }
+    }
+    if (currentState == 3) {
+        return;
+    }
+    chrome.serial.read(connectionId, 1, onSerialRead);
+}
 
 var timer;
 $(document).ready(function() {
@@ -56,7 +111,9 @@ $(document).ready(function() {
             title: title,
             photo1: photo1,
             photo2: photo2,
-            photo23: photo3
+            photo3: photo3,
+            uuid: uuid,
+            physical_address: physicalAddress
         }, function success() {
             $("#newd_form").css({'display': 'none'});
             $("#block_success").css({'display': 'block'});
